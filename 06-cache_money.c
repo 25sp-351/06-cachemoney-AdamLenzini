fe9920269a7cba35342ae7e97074e0ce7634b9b4
AdamLenzini <117232189+AdamLenzini@users.noreply.github.com>
2025-02-24 19:29:38 -0800

    Update 06-cache_money.c

--- 06-cache_money.c
+++ 06-cache_money.c
@@ -2,45 +2,10 @@
 #include <stdlib.h>
 
 #include "text_convert.h"
-
-// Caching
-typedef char* (*char_func_ptr)(long long int param);
+#include "memoization.h"
 
 #define MAX_CENT_AMOUNT 100000000000
-#define MAX_MEMOIZED 150000
-#define ARRAY_SIZE (MAX_MEMOIZED + 1)
-#define UNINITIALIZED NULL
 #define STRING_SIZE 100
-
-char_func_ptr original_provider;
-char* memoized[ARRAY_SIZE];
-
-char* cache_function(long long int num) {
-    if (num < 0 || num > MAX_MEMOIZED) {
-        return (*original_provider)(num);
-        // original_provider(num, money_text);
-        // return;
-    }
-
-    if (memoized[num] == UNINITIALIZED) {
-        memoized[num] = (*original_provider)(num);
-        //snprintf(memoized[num], STRING_SIZE, "%s", (*original_provider)(num));
-        printf("Memoizing %lld: %s\n", num, memoized[num]);
-        //strncpy(memoized[num], (*original_provider)(num), sizeof(memoized[num]));
-    }
-
-    //money_text = memoized[num];
-    return memoized[num];
-}
-
-char_func_ptr initialize(char_func_ptr provider) {
-    for (int ix = 0; ix < ARRAY_SIZE; ix++) {
-        memoized[ix] = UNINITIALIZED;
-    }
-
-    original_provider = provider;
-    return cache_function;
-}
 
 int main(int argc, char **argv) {
     long long cents = 0;
@@ -59,18 +24,12 @@ int main(int argc, char **argv) {
             continue;
         }
 
-        // ProviderFunction money_to_words = cents_to_words;
-
-        // Cache *cache = NULL;
-
-        // cache = load_cache_module;
-
         char* money_text = (char *)malloc(sizeof(char) * STRING_SIZE);
 
         *money_text = '\0';
 
         //money_text = cents_to_words(cents/*, money_text*/);
-        money_text = money_to_words(cents);
+        money_text = (*money_to_words)(cents);
         //money_to_words(cents, money_text);
 
         printf("%s\n", money_text);
